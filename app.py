3ade6cd382adc3b18bd2d0c93943e81d82c5e168
Dante-prog <easton4984@gmail.com>
2023-04-11 11:23:03 -0400

    Changed app message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to the Application which has nothing'
+    return 'Welcome to the Application which has nothing so you can leave'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
