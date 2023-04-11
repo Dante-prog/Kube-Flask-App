2091d6074c75f9a56b477c6da63c61ac4bc32018
Dante-prog <easton4984@gmail.com>
2023-04-11 15:37:38 -0400

    Fixed Incorrect import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from database.py import init_db
+from database import init_db
 
 
 app = Flask(__name__)
