fddde6a5b24c91fde4414ffc77c3787510420dd0
Dante-prog <easton4984@gmail.com>
2023-04-12 13:17:31 -0400

    Adding user-agent to the logs

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ class Post(db.Model):
 @app.before_request
 def log_request_info():
     ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
-    app.logger.info('Request received from %s for %s', ip_address, request.path)
+    app.logger.info('Request received from %s for %s', ip_address, request.path, request.user_agent)
 
 #This is the home route.
 @app.route('/')
